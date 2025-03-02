6e17b6d4fd6423de18ed0eeb1039c02ef7bb64d4
dorjoo <dorjoohover@gmail.com>
2025-03-02 11:21:21 +0800

    'a

--- src/config/app.config.ts
+++ src/config/app.config.ts
@@ -1,8 +1,6 @@
 export default () => ({
-  appSecret: process.env.APP_SECRET,
-  // dbUrl: process.env.DB_URL,
-  dbUrl:
-    'mongodb+srv://dorjoohover:dorjooX0@cluster0.lg7cc.mongodb.net/',
-  // dbName: process.env.DB_NAME,
-  dbName: 'gerelt_tsokh',
-});
+    appSecret: process.env.APP_SECRET,
+    dbUrl: process.env.DB_URL,
+    dbName: process.env.DB_NAME,
+
+  })
